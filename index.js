900fc3f3b89276ed0f61672cbec5984e1bae7b06
shivam Kumar <shivamkumar8171396919@gmail.com>
2025-01-22 21:53:58 +0530

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { Server } = require('socket.io');
 const server = http.createServer(app);
 
 app.use(express.static(path.resolve('public')));
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile('index.html');
 })
 
@@ -15,15 +15,15 @@ const users = {};
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('new-user-joined', (name) => {
-        console.log('print',name)
+        console.log('print', name)
         users[socket.id] = name;
-        socket.broadcast.emit('user-joined',name)
-      });
-    socket.on('send',(message)=>{
-        socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
+        socket.broadcast.emit('user-joined', name)
+    });
+    socket.on('send', (message) => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
     })
-    socket.on('disconnect',message =>{
-        socket.broadcast.emit('left',users[socket.id]);
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     })
 });
